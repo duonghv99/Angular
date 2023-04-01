4de627ac0d8f51ebe4d238e97586c64814304194
duonghv <duonghvph26744@fpt.edu.vn>
2023-04-01 11:02:33 +0700

    angular

--- Bai3_route/controllers/GioiThieuController.js
+++ Bai3_route/controllers/GioiThieuController.js
@@ -1,16 +1,30 @@
-window.GioiThieuController = function ($scope, $routeParams) {
+window.GioiThieuController = function ($scope, $routeParams, $http) {
     // routeParams sẽ ra 1 đôi tượng chứa param trên url
     // console.log($routeParams.name);
     // Tạo 1 đối tượng kiểm tra dữ liệu mặc định là false
+    // Tham số http là giao thức để gọi api
     $scope.kiemTraDuLieu = {
         ten: false, // Chưa có lỗi gì mặc định là false, khi có lỗi đánh dấu thành true
         tuoi: false
+    };
+
+    let apiURL = "http://localhost:3000/posts"; // Điền link api mà mình muốn gọi
+    $scope.getData = function(){
+        $http.get(apiURL).then(function(reponse){
+            // Khi gọi API thành công cục reponse sẽ nhận dữ liệu
+            // console.log(reponse);
+            if(reponse.status == 200){ // Kiểm tra nếu trạng thái == 200 thì thành công
+                $scope.danhsach = reponse.data;
+            }
+        })
     }
 
-    $scope.danhsach = [
-        { id: 1, ten: "Nguyễn Văn A", tuoi: 19 },
-        { id: 2, ten: "Nguyễn Văn B", tuoi: 19 },
-    ]
+    $scope.getData();
+
+    // $scope.danhsach = [
+    //     { id: 1, ten: "Nguyễn Văn A", tuoi: 19 },
+    //     { id: 2, ten: "Nguyễn Văn B", tuoi: 19 },
+    // ]
 
     $scope.onClose = function () {
         $scope.inputValue = {
@@ -49,17 +63,28 @@ window.GioiThieuController = function ($scope, $routeParams) {
                 return;
             }
 
-            let ds = $scope.danhsach;
+            // let ds = $scope.danhsach;
             // fake id tăng tự động
-            let newId = ds.length > 0 ? ds[ds.length - 1].id + 1 : 1;
+            // let newId = ds.length > 0 ? ds[ds.length - 1].id + 1 : 1;
 
             // Tạo object
             let newItem = {
-                id: newId,
+                // id: newId,
                 ten: $scope.inputValue.ten,
                 tuoi: $scope.inputValue.tuoi,
             }
-            $scope.danhsach.push(newItem);
+
+            $http.post(
+                apiURL, // đường dẫn API
+                newItem // dữ liệu thêm
+            ).then(function(reponse){
+                // console.log(reponse);
+                if(reponse.status == 201){
+                    $scope.getData();
+                }
+            })
+            
+            // $scope.danhsach.push(newItem);
             $scope.onClose();
         }
     }
